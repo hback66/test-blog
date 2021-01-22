1b7fc65d44ca5f570ec2ab384eeffcace24aff2f
qwe73 <backmic73@gmail.com>
2021-01-22 23:57:05 +0300

    Added view and template for detailed blog post as well as Css for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', post_list, name='post_list')
+    path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail')
 ]
